1b7f43c41b2c23950670ec9465d8df7dd99bfbdd
Cheesykung <56570717+Cheesykung@users.noreply.github.com>
2022-02-07 20:30:57 +0700

    :wrench: เปลี่ยน db key

--- demo/functions/admin.js
+++ demo/functions/admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./sdtp-ff808-firebase-adminsdk-htbr3-e61858384b.json");
+const serviceAccount = require("./key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
