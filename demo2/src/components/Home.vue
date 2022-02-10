835896f66499c0e22af3972dfc9fb3f314322bb4
EveGalaxy <evez.07@hotmail.com>
2022-02-10 14:44:15 +0700

    :wrench: Update Main page
    สร้าง Component และ จัดหน้าคร่าว ๆ

--- demo2/src/components/Home.vue
+++ demo2/src/components/Home.vue
@@ -1,7 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>Home</h1>
-  </div>
+  <section class="section is-medium">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <div class="columns">
+            <div class="column is-half">
+              <div class="box has-background-grey-lighter">
+                <p class="is-size-4">ไม่รู้จะกินอะไรสินะ งั้น</p>
+                <div class="column is-justify-content-center mb-4">
+                  <button class="button is-info is-medium">สุ่มเลย!</button>
+                </div>
+                <div class="column is-justify-content-center my-4">
+                  <p class="is-size-4">อะไรนะ!</p>
+                  <p class="is-size-4">มีเป้าหมายแล้วหรอ?</p>
+                  <button class="button is-success m-2">ซื้อจากร้าน</button>
+                  <button class="button is-danger m-2">ลองทำเอง</button>
+                </div>  
+              </div>
+            </div>
+            <div class="column is-align-self-center ml-6">
+              <figure class="image is-1by1">
+                  <img alt="cartoon" src="https://freepngimg.com/thumb/my_little_pony/23605-4-rainbow-dash-image.png">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </section>
 </template>
 
 <script>
