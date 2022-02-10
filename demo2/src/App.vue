8e07084e8c38221145133edac178d7b973cc6c52
Cheesykung <56570717+Cheesykung@users.noreply.github.com>
2022-02-10 22:10:55 +0700

    :sparkles: สร้าง HTML ของหน้า Random Display Page
    เพิ่มหน้า HTML Random Display Page
    แต่ยังไม่ได้ใส่ Modal (Pop up) กับ Vue ในนั้น

--- demo2/src/App.vue
+++ demo2/src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-view />
+      <RandomDisplay />
   </div>
 </template>
 
 <script>
+// import RandomDisplay from "@/components/RandomDosplay"
+import RandomDisplay from "./components/RandomDisplay.vue"
+
 export default {
   name: 'App',
+  components: {
+    RandomDisplay
+  }
 }
 </script>
 
 <style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+@import url("https://fonts.googleapis.com/css2?family=Prompt&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Prompt', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ demo2/src/components/RandomDisplay.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="main">
+    <div id="top-bar" class="box mb-0">
+      <div class="level-left">
+        <div id="logo" class="columns level-item">
+          <h1 class="title">Home</h1>
+        </div>
+      </div>
+    </div>
+    <div id="body" class="py-1">
+      <div class="level-item">
+        <div id="title-page" class="level-item mt-2 mb-3">
+          <h1 class="title">Random Food</h1>
+        </div>
+      </div>
+      <div id="list-card" class="columns mb-0">
+        <div v-for="(item, index) in items" :key="index" class="column">
+          <div id="food-card" class="is-4 p-4">
+            <a href="#">
+              <img :src="require('../assets/' + item.url)" alt="pic1" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="level-item">
+        <a href="#">
+          <div id="random-button" class="level-item mt-2 mb-3">
+            <h1 class="title">สุ่มใหม่</h1>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "RandomDisplay",
+  data() {
+    return {
+      items: [
+        { url: "test1.jpeg" },
+        { url: "test2.jpg" },
+        { url: "test3.jpg" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+img {
+  width: 525px;
+  height: 525px;
+  object-fit: cover;
+  border-radius: 20px;
+}
+
+#body {
+  background-color: #6f6f6f;
+}
+
+.box {
+  border-radius: 0px;
+  box-shadow: none;
+  background-color: #d0d0d0;
+}
+
+#title-page {
+  background-color: white;
+  height: 100px;
+  width: 976px;
+  border-radius: 50px;
+}
+
+#logo {
+  background-color: #acacac;
+  height: 120px;
+  width: 190px;
+}
+
+#food-card {
+  background-color: white;
+  border-radius: 20px;
+}
+
+#random-button {
+  background-color: white;
+  height: 100px;
+  width: 567px;
+  border-radius: 50px;
+}
+</style>
