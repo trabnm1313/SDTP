257fd0fc7634388830a1b5b12aafd3fb0836cf23
trabnm1313 <jame.sou.com@hotmail.com>
2022-02-11 11:38:04 +0700

    :sparkles: RandomMenuAPI

--- demo2/functions/route/randomMenuAPI.js
+++ demo2/functions/route/randomMenuAPI.js
@@ -1,13 +1,45 @@
 const Express = require('express')
 const router = Express.Router()
 
+const admin = require("../admin")
+const db = admin.firestore()
+const menuRef = db.collection("Menu")
+
 router.use(Express.json())
 
 router.get("/", (req, res) => {
-    console.log("Console logging!")
-    res.status(200).json({
-        message: "OK"
-    })
+
+    const tags = req.body["tags"]
+
+    if(tags.length != 0){
+        menuRef.where("tags", "array-contains-any", tags).get().then(querySnapshot => {
+            let tempDataList = []
+            querySnapshot.forEach(menu => {
+                tempDataList.push(menu.data()["tags"])
+            })
+
+            let randomMenu = []
+            let randomNumber = 0
+            let maxRandomNumber = tempDataList.length >= 3 ? 3 : tempDataList.length
+            for(let num=0; num<maxRandomNumber; num++){
+                randomNumber = Math.floor(Math.random() * tempDataList.length)
+                randomMenu.push(tempDataList[randomNumber])
+                tempDataList.splice(randomNumber, 1)
+            }
+
+            res.status(200).json({
+                message: "OK",
+                menu: randomMenu
+            })
+        }).catch(error => {
+            console.log(error)
+            res.status(400).json({
+                message: "Something went wrong.",
+                err: error
+            })
+        })
+    }
+
 })
 
 module.exports = router
