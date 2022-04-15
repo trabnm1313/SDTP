67f016a517429bcdb14b46c06f5948b2870097bb
EveGalaxy <evez.07@hotmail.com>
2022-04-16 00:27:43 +0700

    :sparkles: เพิ่มหน้า Detail Recipe
    เพิ่ม component หน้า Detail Recipe

---
+++ frontend/src/components/DetailRecipe.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-10">
+          <div class="columns">
+            <div class="column is-8 mt-2">
+              <div class="box has-background-grey-lighter">
+                <figure class="image is-4by3">
+                  <img
+                    alt="foodimage"
+                    src="https://food.grab.com/static/page-home/TH-new-3.jpg"
+                  />
+                </figure>
+                
+              </div>
+              <button class="button is-dark"  @click="goRecipe()">
+                BACK :)
+              </button>
+            </div>
+            <div class="column is-6 ml-6">
+                <h2 class="title is-2">ชื่อเมนู</h2>
+                <div class="box has-background-grey-lighter">
+                    <h4 class="title is-4">ส่วนผสม</h4>
+                    <div class="column is-6">
+                        <div class="content">
+                            <ol type="1">
+                                <li>เพิ่มส่วนผสมตรงนี้</li>
+                            </ol>
+                        </div> 
+                    </div>
+                </div>
+                <div class="box has-background-grey-lighter">
+                    <h4 class="title is-4">วิธีทำ</h4>
+                    <div class="column is-6">
+                        <div class="content">
+                            <ol type="1">
+                                <li>เพิ่มวิธีทำตรงนี้</li>
+                            </ol>    
+                        </div>     
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "DetailRecipe",
+  methods: {
+    goRecipe(){
+      this.$router.push('/SearchRecipe')
+    },
+  }
+};
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../components/Home.vue";
 import FilterMenu from "../components/FilterMenu.vue";
 import RandomDisplay from "../components/RandomDisplay.vue"
 import SearchRecipe from "../components/SearchRecipe.vue"
+import DetailRecipe from "../components/DetailRecipe.vue"
 
 
 const routes = [
@@ -26,8 +27,12 @@ const routes = [
       path: "/SearchRecipe",
       name: "SearchRecipe",
       component: SearchRecipe
+    },
+    {
+      path: "/DetailRecipe",
+      name: "DetailRecipe",
+      component: DetailRecipe
     }
-
 ];
 
 const router = createRouter({
